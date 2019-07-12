e81f5b9c35f0d27bd2e1c29aa5657bf1841bfd87
Lincoln Bryant <lincolnb@uchicago.edu>
2019-07-12 10:22:18 -0500

    Updated verbose flag

--- supervisord_startup.sh
+++ supervisord_startup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Run configuration
-globus-connect-server-setup
+globus-connect-server-setup -v
 
 # Now we can actually start the supervisor
 exec /usr/bin/supervisord -c /etc/supervisord.conf
