e2be426976009ca99374e85c90c6702debce6ade
Lincoln Bryant <lincolnb@uchicago.edu>
2019-07-12 13:28:21 -0500

    updated to just start scripts as normal and then sleep

--- supervisord_startup.sh
+++ supervisord_startup.sh
@@ -3,5 +3,11 @@
 # Run configuration
 globus-connect-server-setup -v
 
+# now we just run the init scripts and fork em off
+/etc/init.d/myproxy-server start
+/etc/init.d/globus-gridftp-server start
+
+while true; do sleep 3600; done
+
 # Now we can actually start the supervisor
-exec /usr/bin/supervisord -c /etc/supervisord.conf
+#exec /usr/bin/supervisord -c /etc/supervisord.conf
