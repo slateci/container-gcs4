bf86e0caf34c682369942c10cd938fb6613f9f49
Lincoln Bryant <lincolnb@uchicago.edu>
2019-07-12 16:41:59 -0500

    added some defensiveness

--- supervisord_startup.sh
+++ supervisord_startup.sh
@@ -7,6 +7,11 @@ if [[ $? -eq 0 ]]; then
 	IFS=':'
 	# loop through every line of the config map / passwd file and create the user,
 	# unlock them, and add their public key
+    cp /root/passwd /root/passwd1
+    # small sanitization of the passwd file.
+    # kubernetes will strip newlines, but we need at least one so the while 
+    # loop can read the file.  this strips any empty lines after..
+    sed -i '/^$/d' /tmp/passwd
 	while read -r user pass uid gid comment home shell; do
 		echo "username is: " $user
 		echo "password is: " $pass
@@ -16,14 +21,18 @@ if [[ $? -eq 0 ]]; then
 		echo "home is: " $home
 		echo "shell is: " $shell
 
-		if [[ $pass -eq "" ]]; then
+		if [[ $pass == "" ]]; then
 			echo "password seems to be empty.. cowardly refusing to continue"
 			break
 		fi
-
 		useradd $user -d $home -u $uid -g $gid -s $shell -p $pass
-	done < /root/passwd
+
+	done < /root/passwd1
 fi
+
+# clean up any of these in case the file leaks out of gridftp
+rm -f /root/passwd
+rm -f /root/passwd1
 
 # Run configuration
 globus-connect-server-setup -v
